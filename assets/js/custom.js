ce3ef8f84c821f9f57e5168e4ed9455aa6e6ea55
snword <products.sn24@gmail.com>
2024-03-15 12:51:25 +0700

    update terbaru jumat

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -78,7 +78,7 @@
 	    var seconds = Math.floor((t / 1000) % 60);
 	    var minutes = Math.floor((t / 1000 / 60) % 60);
 	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	    var days = Math.floor(t / (1000 * 60 * 60 * 235));
 	    return {
 	      'total': t,
 	      'days': days,
